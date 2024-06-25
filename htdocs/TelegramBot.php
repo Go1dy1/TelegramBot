f201fc89b1eee091ef2781cdb2d5a30ba1a6b643
Go1dy1 <art.nik7pup@gmail.com>
2024-06-25 04:16:50 +0300

    Dev

--- htdocs/TelegramBot.php
+++ htdocs/TelegramBot.php
@@ -1,46 +1,30 @@
 <?php
+require_once "config.php";
+require_once "DbUserStorage.php";
+require_once 'telegramUserManager.php';
 /**
  * @method getMe()
- * @method sendMessage($params)
- * @method sendPhoto($params)
- * @method sendAudio($params)
- * @method sendVideo($params)
- * @method sendAnimation($params)
- * @method sendVoice($params)
+ * @method sendMessage($chat_id,$params)
+ * @method sendPhoto($chat_id,$params)
+ * @method sendAudio($chat_id,$params)
+ * @method sendVideo($chat_id,$params)
+ * @method sendAnimation($chat_id,$params)
+ * @method sendVoice($chat_id,$params)
+ * @method getUpdates($params)
  *
  */
 
-class TelegramBot
-{
+class TelegramBot {
     private $token;
     private $api_url = 'https://api.telegram.org/bot';
-    private $pdo;
+    public $db;
+    public $telegramUserManager;
 
-    public function __construct($token, $db_config)
-    {
+    public function __construct($token) {
         $this->token = $token;
-        $this->connectDatabase($db_config);
     }
 
-    private function connectDatabase($db_config)
-    {
-        $dsn = "mysql:host={$db_config['host']};dbname={$db_config['dbname']};charset=utf8mb4";
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ];
-        try {
-            $this->pdo = new PDO($dsn, $db_config['user'], $db_config['pass'], $options);
-        }
-        catch (PDOException $e)
-        {
-            die('Database connection failed: ' . $e->getMessage());
-        }
-    }
-
-    private function makeRequest($endpoint, $params = [], $isMultipart = false)
-    {
+    private function makeRequest($endpoint, $params = [], $isMultipart = false) {
         $url = $this->api_url . $this->token . '/' . $endpoint;
 
         $ch = curl_init($url);
@@ -51,22 +35,52 @@ class TelegramBot
         if ($isMultipart) {
             curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: multipart/form-data']);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-        }
-        else {
+        } else {
             $payload = json_encode($params);
             curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
         }
 
         $response = curl_exec($ch);
-        curl_close($ch);
 
+        if (curl_errno($ch)) {
+            error_log('Curl error: ' . curl_error($ch));
+        }
+
+        curl_close($ch);
         return json_decode($response);
     }
 
-    public function __call($name, $arguments)
+    public function __call($name, $arguments) {
+        $params = $arguments[0];
+
+        if (!isset($params['chat_id'])) {
+            throw new InvalidArgumentException("chat_id is required in arguments for method $name");
+        }
+
+        if (isset($params['media'])) {
+            $params[$this->getMediaType($name)] = new CURLFile(realpath($params['media']));
+            $this->makeRequest($name, $params, true);
+        } else {
+            $this->makeRequest($name, $params, false);
+        }
+    }
+
+    private function getMediaType($methodName) {
+        $mediaTypes = [
+            'sendPhoto' => 'photo',
+            'sendAudio' => 'audio',
+            'sendVideo' => 'video',
+            'sendAnimation' => 'animation',
+            'sendVoice' => 'voice'
+        ];
+        return $mediaTypes[$methodName] ?? null;
+    }
+
+    /*
+      public function __call($name, $arguments)
     {
-        $stmt = $this->pdo->query('SELECT telegram_id FROM users');
+        $stmt = $pdo->query('SELECT telegram_id FROM users');
         $users = $stmt->fetchAll(PDO::FETCH_COLUMN);
 
         foreach ($users as $user_id) {
@@ -81,22 +95,5 @@ class TelegramBot
             }
         }
     }
-
-    private function getMediaType($methodName)
-    {
-        $mediaTypes = [
-            'sendPhoto' => 'photo',
-            'sendAudio' => 'audio',
-            'sendVideo' => 'video',
-            'sendAnimation' => 'animation',
-            'sendVoice' => 'voice'
-        ];
-        return $mediaTypes[$methodName] ?? null;
-    }
-
-    public function getUsers()
-    {
-        $stmt = $this->pdo->query('SELECT telegram_id FROM users');
-        return $stmt->fetchAll(PDO::FETCH_COLUMN);
-    }
+     */
 }
---
+++ htdocs/telegramUserManager.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * @method $storage UserStorageInterface
+ */
+class telegramUserManager {
+    public $storage;
+    public $bot;
+
+    public function __construct( TelegramBot $bot, DbUserStorage $storage) {
+        $this->bot = $bot;
+        $this->storage = $storage;
+    }
+    public function fetchAndStoreUsers() {
+        // Получение обновлений от Telegram
+        $response = $this->bot->getUpdates([]);
+
+        $unique_users = [];
+
+        if (isset($response['result'])) {
+            foreach ($response['result'] as $update) {
+                if (isset($update['message']['from']['id'])) {
+                    $user_id = $update['message']['from']['id'];
+                    $user_name = $update['message']['from']['username'] ?? 'No username';
+
+                    if (!isset($unique_users[$user_id])) {
+                        $unique_users[$user_id] = $user_name;
+                    }
+                }
+            }
+
+            foreach ($unique_users as $id => $username) {
+                $this->storage->storeUser($id, $username);
+            }
+        } else {
+            echo "No updates found.";
+        }
+    }
+
+
+    public function getUsers()
+    {
+        return $this->storage->getUsers();
+    }
+    public function getUsersName()
+    {
+        return $this->storage->getUsers();
+    }
+
+}
+
+?>
